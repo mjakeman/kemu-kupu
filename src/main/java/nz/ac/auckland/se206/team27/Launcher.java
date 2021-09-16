7011155e04486e2da67e001c48eb117362c7a31c
Raymond Feng <rf.raymondfeng@gmail.com>
2021-09-17 01:10:17 +1200

    feat Add ability to compile standalone fat jar (#7)

---
+++ src/main/java/nz/ac/auckland/se206/team27/Launcher.java
@@ -0,0 +1,14 @@
+package nz.ac.auckland.se206.team27;
+
+/**
+ * Default launcher for creating an executable jar file.
+ *
+ * @author Raymond Feng (rf.raymondfeng@gmail.com)
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+
+}
