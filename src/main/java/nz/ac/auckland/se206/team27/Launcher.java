867e3e7a09206839b687eb25b353fa7dc50d86a5
Raymond Feng <rf.raymondfeng@gmail.com>
2021-09-17 02:54:44 +1200

    fix Reorder property setting (#7)

--- src/main/java/nz/ac/auckland/se206/team27/Launcher.java
+++ src/main/java/nz/ac/auckland/se206/team27/Launcher.java
@@ -6,12 +6,12 @@ package nz.ac.auckland.se206.team27;
 public class Launcher {
 
     public static void main(String[] args) {
-        App.startApplication(args);
-
         // Add GTK version 2 flag if we are on a Linux distribution
         if (System.getProperty("os.name").toLowerCase().contains("linux")) {
             System.setProperty("jdk.gtk.version", "2");
         }
+
+        App.startApplication(args);
     }
 
 }
