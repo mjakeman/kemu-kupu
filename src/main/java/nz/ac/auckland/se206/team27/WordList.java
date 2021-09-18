054a49e5bf34de7d87e0d8eca9662653bde8c0da
Raymond Feng <rf.raymondfeng@gmail.com>
2021-09-19 01:04:46 +1200

    Make WordList not throw an exception on constructor (#17)

--- src/main/java/nz/ac/auckland/se206/team27/WordList.java
+++ src/main/java/nz/ac/auckland/se206/team27/WordList.java
@@ -8,16 +8,16 @@ import java.util.Scanner;
 /**
  * @author Jordan York (jyor212@aucklanduni.ac.nz)
  */
-
 public class WordList {
+
     //Create fields to hold topic title, description and wordlist
     private String title;
     private String description;
     private ArrayList<String> wordList;
 
-    public WordList(String topic) throws FileNotFoundException {
+    public WordList(String fileName) {
         //Locate file of topic
-        String filePath = new File("").getAbsolutePath() + "/words/" + topic + ".txt";
+        String filePath = new File("").getAbsolutePath() + "/words/" + fileName;
         File topicFile = new File(filePath);
 
         //Set wordlist to be new
@@ -26,28 +26,36 @@ public class WordList {
         //Create string to hold each word in the list
         String wordForLoop;
 
-        //Create new scanner to start reading the file
-        Scanner fileScanner = new Scanner(topicFile);
+        try {
+            //Create new scanner to start reading the file
+            Scanner fileScanner = new Scanner(topicFile);
 
-        //First line in file is topic so read it and assign to title field
-        title = fileScanner.nextLine();
+            //First line in file is topic so read it and assign to title field
+            title = fileScanner.nextLine();
 
-        //Second line in file is description so read it and assign to description field
-        description = fileScanner.nextLine();
+            //Second line in file is description so read it and assign to description field
+            description = fileScanner.nextLine();
 
-        //The remaining lines in file are words to be spelt. Read all of these and add them to wordlist field
-        while (fileScanner.hasNext()) {
-            wordForLoop = fileScanner.nextLine();
-            wordList.add(wordForLoop);
+            //The remaining lines in file are words to be spelt. Read all of these and add them to wordlist field
+            while (fileScanner.hasNext()) {
+                wordForLoop = fileScanner.nextLine();
+                wordList.add(wordForLoop);
+            }
+
+            //Stop reading file
+            fileScanner.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("[Error] File \"" + fileName + "\" was not found!");
         }
-
-        //Stop reading file
-        fileScanner.close();
     }
 
     public ArrayList<String> getWordList() {
 
         //Retrieve wordlist
         return wordList;
+    }
+
+    public String getTitle() {
+        return title;
     }
 }
