a45a6eb603a1f7e70710ae8fd91c3fdafa122f28
Raymond Feng <rf.raymondfeng@gmail.com>
2021-10-23 17:38:44 +1300

    Refactor ResultController.java populateViewModel() method (#88)
    Signed-off-by: Raymond Feng <rf.raymondfeng@gmail.com>

--- src/main/java/nz/ac/auckland/se206/team27/controller/ResultController.java
+++ src/main/java/nz/ac/auckland/se206/team27/controller/ResultController.java
@@ -1,5 +1,7 @@
 package nz.ac.auckland.se206.team27.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -12,6 +14,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import nz.ac.auckland.se206.team27.SoundManager;
 import nz.ac.auckland.se206.team27.controller.base.GameController;
+import nz.ac.auckland.se206.team27.game.RoundResult;
 import nz.ac.auckland.se206.team27.resource.AudioResource;
 import nz.ac.auckland.se206.team27.resource.ScreenResource;
 import nz.ac.auckland.se206.team27.util.JavaFXUtil;
@@ -58,6 +61,42 @@ public class ResultController extends GameController {
     private TimerTask nextRoundTimerTask;
 
     /**
+     * Define and populate a list of messages for each different round result.
+     */
+    private static final Map<RoundResult, String[]> encouragingMessages = new HashMap<>();
+    static {
+        String[] passed = {
+                "Ka pai! On the first try",
+                "Tino Pai! Awesome work",
+                "Great job! You smashed it",
+        };
+
+        String[] faulted = {
+                "Not bad, keep it up!",
+                "Well done! Perseverance is key",
+                "Good job for sticking with it"
+        };
+
+        String[] failed = {
+                "Give it a go next time",
+                "Have a crack at it next time",
+                "Go for it next time"
+        };
+
+        String[] skipped = {
+                "Keep putting in the mahi, you'll get it one day!",
+                "Kia Kaha! Keep going",
+                "Don't give up, keep building"
+        };
+
+        encouragingMessages.put(RoundResult.FAILED, failed);
+        encouragingMessages.put(RoundResult.FAULTED, faulted);
+        encouragingMessages.put(RoundResult.PASSED, passed);
+        encouragingMessages.put(RoundResult.SKIPPED, skipped);
+    }
+
+
+    /**
      * Action executed when the "Skip" button is clicked.
      */
     public void clickNext() {
@@ -83,99 +122,43 @@ public class ResultController extends GameController {
     /**
      * Data that is populated when this controller is loaded.
      */
-    // TODO: Refactor this
     @Override
     protected void populateViewData() {
         ResultScreenDto data = gameViewModel.getResultScreenData();
-
-        String[] encouragingMsgPassed = new String[3];
-        String[] encouragingMsgFaulted = new String[3];
-        String[] encouragingMsgFailed = new String[3];
-        String[] encouragingMsgSkipped = new String[3];
-        String encouragingMsg;
-        String resultMsg;
-        String styleClass;
-        Random passedRand = new Random();
-        AudioResource soundClip;
-
-        switch (data.resultFromLastRound)
-        {
-            case PASSED:
-                resultMsg = "Correct!";
-                encouragingMsgPassed[0] = "Ka pai! On the first try";
-                encouragingMsgPassed[1] = "Tino Pai! Awesome work";
-                encouragingMsgPassed[2] = "Great job! You smashed it";
-                while (passedRand.nextInt() < 0) {
-                    passedRand = new Random();
-                }
-                encouragingMsg = encouragingMsgPassed[passedRand.nextInt(3)];
-                styleClass = "answer-correct";
-                soundClip = AudioResource.CORRECT;
-                break;
-
-            case FAULTED:
-                resultMsg = "Correct!";
-                encouragingMsgFaulted[0] = "Not bad, keep it up!";
-                encouragingMsgFaulted[1] = "Well done! Perseverance is key";
-                encouragingMsgFaulted[2] = "Good job for sticking with it";
-                while (passedRand.nextInt() < 0) {
-                    passedRand = new Random();
-                }
-                encouragingMsg = encouragingMsgFaulted[passedRand.nextInt(3)];
-                styleClass = "answer-correct";
-                soundClip = AudioResource.CORRECT;
-                break;
-
-            case SKIPPED:
-                resultMsg = "Skipped";
-                encouragingMsgSkipped[0] = "Give it a go next time";
-                encouragingMsgSkipped[1] = "Have a crack at it next time";
-                encouragingMsgSkipped[2] = "Go for it next time";
-                while (passedRand.nextInt() < 0) {
-                    passedRand = new Random();
-                }
-                encouragingMsg = encouragingMsgSkipped[passedRand.nextInt(3)];
-                styleClass = "answer-skipped";
-                soundClip = AudioResource.SKIPPED;
-                break;
-
-            case FAILED:
-                resultMsg = "Incorrect :(";
-                encouragingMsgFailed[0] = "Keep putting in the mahi, you'll get it one day!";
-                encouragingMsgFailed[1] = "Kia Kaha! Keep going";
-                encouragingMsgFailed[2] = "Don't give up, keep building";
-                while (passedRand.nextInt() < 0) {
-                    passedRand = new Random();
-                }
-                encouragingMsg = encouragingMsgFailed[passedRand.nextInt(3)];
-                styleClass = "answer-incorrect";
-                soundClip = AudioResource.INCORRECT;
-                break;
-
-            default:
-                throw new IllegalArgumentException();
-        }
 
         if (data.isPracticeMode) {
             JavaFXUtil.toggleNodeVisibility(scoreContainer, false);
             JavaFXUtil.toggleNodeVisibility(labelPlusScore, false);
         }
 
-        labelResult.setText(resultMsg);
-        labelEncouragement.setText(encouragingMsg);
-        answerContainer.getStyleClass().add(styleClass);
+        // Convert the round result to human form (i.e. result name, encouragement, audio and colour)
+        ResultDisplayDto resultDisplay = getRandomResult(data.resultFromLastRound);
+        labelResult.setText(resultDisplay.resultMessage);
+        labelEncouragement.setText(resultDisplay.encouragingMessage);
+        answerContainer.getStyleClass().add(resultDisplay.styleClass);
+        SoundManager.getInstance().playClip(resultDisplay.soundClip);
 
         labelTotalScore.setText("" + data.currentScore);
         labelPlusScore.setText(String.format("(+%d points from this round)", data.scoreAddedFromLastRound));
+
         answer.setText(data.word);
 
-        String btnText = (data.hasNextWord) ? "Next Word" : "Results";
+        // Automate the button countdown from 10 seconds and change the prefix depending
+        // on whether there is another word left
+        setButtonCountdown((data.hasNextWord) ? "Next Word" : "Results", 10);
 
-        SoundManager.getInstance().playClip(soundClip);
+    }
 
+    /**
+     * Sets up a timer for automatically progressing to the next screen after
+     * the number of seconds elapses.
+     *
+     * @param seconds The number of seconds elapsed before moving to the next screen.
+     */
+    private void setButtonCountdown(String buttonPrefix, int seconds) {
         nextRoundTimerTask = new TimerTask() {
 
-            private int secondsPast = 10;
+            private int secondsPast = seconds;
 
             @Override
             public void run() {
@@ -186,15 +169,69 @@ public class ResultController extends GameController {
                         return;
                     }
 
-                    btnNext.setText(btnText + " (" + (secondsPast + 1) + "s)");
+                    btnNext.setText(buttonPrefix + " (" + (secondsPast + 1) + "s)");
                 });
             }
         };
 
         Timer nextScreenTimer = new Timer();
         nextScreenTimer.schedule(nextRoundTimerTask, 0,1000L);
+    }
 
-        btnNext.setText(btnText);
+    /**
+     * @return a randomly selected message based on the result.
+     */
+    private static ResultDisplayDto getRandomResult(RoundResult result) {
+        // List of possible messages given the round's result
+        String[] possibleMsgs = encouragingMessages.get(result);
+
+        // Get a random encouraging message from the list of possible messages
+        int randIndex = new Random().nextInt(possibleMsgs.length);
+        String encouragingMsg = possibleMsgs[randIndex];
+
+        switch (result) {
+            case SKIPPED:
+                return new ResultDisplayDto("Skipped",
+                                            encouragingMsg,
+                                            AudioResource.SKIPPED,
+                                            "answer-skipped");
+            case FAILED:
+                return new ResultDisplayDto("Incorrect :(",
+                                            encouragingMsg,
+                                            AudioResource.INCORRECT,
+                                            "answer-incorrect");
+            // Faulted and passed both have the same styling and other attributes
+            case FAULTED:
+            case PASSED:
+                return new ResultDisplayDto("Correct",
+                                            encouragingMsg,
+                                            AudioResource.CORRECT,
+                                            "answer-correct");
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * Data class for storing information related to displaying the result.
+     */
+    private static class ResultDisplayDto {
+
+        public final String resultMessage;
+        public final String encouragingMessage;
+        public final AudioResource soundClip;
+        public final String styleClass;
+
+        public ResultDisplayDto(String resultMessage,
+                                String encouragingMessage,
+                                AudioResource soundClip,
+                                String styleClass) {
+            this.resultMessage = resultMessage;
+            this.encouragingMessage = encouragingMessage;
+            this.soundClip = soundClip;
+            this.styleClass = styleClass;
+        }
+
     }
 
 }
